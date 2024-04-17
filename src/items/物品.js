9e92bc1a11ca3390c80b5a7c15593c24d8bb9803
ShadowCK <ShadowCK@users.noreply.github.com>
2024-04-17 14:05:21 +0000

    deploy: ba506daf8c49d76b8cf42ac3e383a607f5200b74

---
+++ src/items/物品.js
@@ -0,0 +1,21 @@
+class 物品 {
+  name = '玻璃瓶';
+
+  description = '一个玻璃瓶。';
+
+  stackable = true;
+
+  stack = 1;
+
+  maxStack = 64;
+
+  config = null;
+
+  constructor(itemConfig, count) {
+    this.config = itemConfig;
+    Object.assign(this, itemConfig);
+    this.stack = count;
+  }
+}
+
+export default 物品;
