764b71c19c02f0ce4b9600acaa5a478bf260543c
Zhao Jin <540712348@qq.com>
2024-04-19 06:34:06 -0400

    网页在后台时玩家死亡有不同的死亡提示

--- src/events/htmlHandler.js
+++ src/events/htmlHandler.js
@@ -8,18 +8,45 @@ const clearEntityInCombatTab = (entity) => {
   战斗面板实体列表.find(`#${entity.uuid}`).remove();
 };
 
+let isUserOnPage = true;
+let isDeathPopupVisible = false;
+
+$(document).on('visibilitychange', () => {
+  if (document.visibilityState === 'visible') {
+    isUserOnPage = true;
+  } else if (document.visibilityState === 'hidden') {
+    isUserOnPage = false;
+  }
+});
+
 const registerEvents = () => {
   combatEvents.on(EventType.实体死亡, ({ entity }) => {
     if (entity !== getPlayer()) {
       return;
     }
-    $.toast({
-      displayTime: 2000,
-      class: 'error chinese',
-      showProgress: 'bottom',
-      title: '你死了！',
-      message: `真是个菜鸡`,
-    });
+    if (isUserOnPage) {
+      $.toast({
+        title: '你死了！',
+        message: `真是个菜鸡。`,
+        class: 'error chinese',
+        displayTime: 2000,
+        showProgress: 'bottom',
+      });
+    } else if (!isDeathPopupVisible) {
+      // 页面在后台时死亡，永久显示popup。直到点掉，不会再生成新的死亡提示，防止每次死亡弹出一个需要手动点掉的提示。
+      $.toast({
+        title: '你在不知不觉的时候死了！',
+        message: `试试换个区域挂机吧。`,
+        class: 'black chinese',
+        displayTime: 0,
+        onShow: () => {
+          isDeathPopupVisible = true;
+        },
+        onHidden: () => {
+          isDeathPopupVisible = false;
+        },
+      });
+    }
     changeTab('角色面板');
   });
 
