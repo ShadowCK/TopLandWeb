0f35846ac61bba927b5365448c5c3155a5f88246
Zhao Jin <540712348@qq.com>
2024-04-21 14:44:56 -0400

    添加了导出/导入存档的功能。

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
-import webpack from 'webpack'; // eslint-disable-line import/no-extraneous-dependencies
+import webpack from 'webpack';
 
 // Note: Cannot use __dirname and __filename in Node.js's ES module,
 const __filename = fileURLToPath(new URL(import.meta.url));
 const __dirname = dirname(__filename);
+
+const config = JSON.parse(fs.readFileSync(resolve(__dirname, './package.json')));
+const { version } = config;
+console.log(version);
 
 export default (env, argv) => {
   const mode = argv.mode || 'development';
@@ -21,6 +26,7 @@ export default (env, argv) => {
     plugins: [
       new webpack.DefinePlugin({
         WEBPACK_MODE: JSON.stringify(mode === 'production' ? 'production' : 'development'),
+        VERSION: JSON.stringify(version),
       }),
     ],
   };
