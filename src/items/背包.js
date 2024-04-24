bad9d298fc0195671d238cd8d4b719cbb78a7cb2
jye110 <1537309753@qq.com>
2024-04-24 05:55:01 -0700

    重构背包1/n 修复可堆叠物品和部分物品事件

--- src/items/背包.js
+++ src/items/背包.js
@@ -17,13 +17,13 @@ class 背包 {
 
   loadSavedItems(items, doClear = true) {
     if (doClear) {
-      this.items.length = 0;
+      this.removeAll();
     }
     items.forEach((itemConfig) => {
       this.addItemFromConfig(itemConfig);
     });
   }
-
+  
   addItemFromConfig(itemConfig, count = 1) {
     if (itemConfig.type === ItemType.装备) {
       this.addItem(new 装备(itemConfig), count);
@@ -43,46 +43,51 @@ class 背包 {
       _.times(count, () => {
         const index = this.items.push(new item.constructor(item.config)) - 1;
         generalEvents.emit(EventType.获得物品, {
+          container: this,
           index,
+          item,
+          stack: 1,
           prevLength,
         });
-      });      
+      });
       return;
     }
-    const existingItem = this.items.find((other) => other.name === item.name);
+
     const stacksToAdd = item.stack * count;
     let stackLeft = stacksToAdd;
-    let index = null;
-    if (existingItem) {
-      const added = Math.min(stackLeft, existingItem.maxStack - existingItem.stack);
-      index = this.items.indexOf(existingItem);
-      existingItem.stack += added;
-      stackLeft -= added;
-      if (stackLeft === 0) {
+    // 堆叠在已有物品
+    for (let i = 0; i < this.items.length; i++) {
+      if (stackLeft <= 0) {
+        break;
+      }
+      const existingItem = this.items[i];
+      if (existingItem.name === item.name) {
+        const added = Math.min(stackLeft, existingItem.maxStack - existingItem.stack);
+        existingItem.stack += added;
+        stackLeft -= added;
         generalEvents.emit(EventType.获得物品, {
-          config: item.config,
-          stack: stacksToAdd,
-          index,
+          container: this,
+          index: i,
+          item: existingItem,
+          stack: added,
           prevLength,
         });
-        return;
       }
     }
-    const startIndex = this.items.length;
+    // 作为新物品
     while (stackLeft > 0) {
-      const newItem = new 物品(item.config);
+      const newItem = new item.constructor(item.config);
       newItem.stack = Math.min(stackLeft, newItem.maxStack);
       stackLeft -= newItem.stack;
       this.items.push(newItem);
+      generalEvents.emit(EventType.获得物品, {
+        container: this,
+        index: this.items.length - 1,
+        item: newItem,
+        stack: newItem.stack,
+        prevLength,
+      });
     }
-    generalEvents.emit(EventType.获得物品, {
-      config: item.config,
-      stack: stacksToAdd,
-      index,
-      startIndex,
-      endIndex: this.items.length - 1,
-      prevLength,
-    });
   }
 
   removeItemAt(index) {
@@ -92,12 +97,8 @@ class 背包 {
       console.error('Trying to remove an item that is not in the bag');
       return;
     }
-    if (!item.stackable || item.stack === 1) {
-      this.items.splice(index, 1);
-    } else {
-      item.stack -= 1;
-    }
-    generalEvents.emit(EventType.失去物品, { config: item.config, stack: 1, index, prevLength });
+    this.items.splice(index, 1);
+    generalEvents.emit(EventType.失去物品, { container: this, index, item, prevLength });
   }
 
   /**
@@ -121,10 +122,10 @@ class 背包 {
     this.removeItemAt(index);
   }
 
-  clear() {
-    const itemConfigs = this.items.map((item) => item.config);
-    this.items.length = 0;
-    generalEvents.emit(EventType.失去物品, itemConfigs);
+  removeAll() {
+    for (let i = this.items.length - 1; i >= 0; i--) {
+      this.removeItemAt(i);
+    }
   }
 }
 
